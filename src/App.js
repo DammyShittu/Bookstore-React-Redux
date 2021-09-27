d8c87ffdc6992ccd798d118686c49f6f8a70c825
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-09-27 15:15:01 +0100

    Add Routes To Links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Route, Switch } from 'react-router-dom';
+import Books from './redux/Books/Books';
+import Categories from './redux/Categories/Categories';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-    </div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Books />
+          </Route>
+          <Route path="/categories">
+            <Categories />
+          </Route>
+        </Switch>
+      </div>
   );
 }
 
