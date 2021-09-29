0d6d6b705cb6a161b32dc709f2b824f81f723784
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-09-30 00:20:15 +0100

    Import useEffect and Dispatch getBooks

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import { addBook, removeBook } from '../redux/Books/Books';
+import { getBooks } from '../redux/Books/Books';
 import Book from './Book';
 import BookInput from './BookInput';
 
@@ -10,22 +9,15 @@ const AllBooks = () => {
 
   const dispatch = useDispatch();
 
-  const addNew = (title, author) => {
-    const newBook = {
-      id: uuidv4(),
-      title,
-      author,
-    };
-    dispatch(addBook(newBook));
-  };
-
-  const deleteOneBook = (id) => dispatch(removeBook(id));
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
 
   if (myBooks.length) {
     return (
       <div>
-        <Book books={myBooks} propsToRemoveBooks={deleteOneBook} />
-        <BookInput propsToAddBooks={addNew} />
+        <Book books={myBooks} />
+        <BookInput />
       </div>
     );
   }
@@ -33,7 +25,7 @@ const AllBooks = () => {
     <div>
       <h3>No Books Yet...😔</h3>
       <h3>Please type here 👇 to add your favorite books.</h3>
-      <BookInput propsToAddBooks={addNew} />
+      <BookInput />
     </div>
   );
 };
