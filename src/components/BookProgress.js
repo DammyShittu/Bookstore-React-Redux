6e287a5d0af819ff271e7a2c7b233706d6fb91fd
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-10-01 12:34:13 +0100

    Create BookProgress and BookChapter Components and Add Relevant Code

---
+++ src/components/BookProgress.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BookProgress = () => (
+  <div className="progress">
+    <div className="progress-img" />
+    <div>
+      <p className="percent">62%</p>
+      <p className="completed">Completed</p>
+    </div>
+  </div>
+);
+
+export default BookProgress;
